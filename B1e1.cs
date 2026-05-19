bd200f16249b4f0e5e3934a13a21cfb6bfe827fe
ganampa27-ctrl <ganampa27@gmail.com>
2026-05-18 23:32:11 -0500

    Add files via upload

---
+++ B1e1.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class B1e1 : MonoBehaviour
+{
+   
+    void Start()
+    {
+        for (int i = 0; i <= 10; i++)
+        {
+            print(i);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ B1e2.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class B1e2 : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        for (int i = 10; i >= 0; i--)
+        {
+            print(i);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
